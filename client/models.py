fad86934223cd15a225d3316161a4f259471e8ef
Kyungwook Cho <amisay5256@naver.com>
2018-07-02 22:56:43 +0900

    Account, Menu, Favor 모델 추가

--- client/models.py
+++ client/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    account_id = models.AutoField(primary_key=True)
+    account_email = models.CharField(max_length=40, unique=True, null=False)
+    account_pw = models.CharField(max_length=20, null=False)
+
+class Menu(models.Model):
+    menu_id = models.AutoField(primary_key=True)
+    menu_name = models.CharField(max_length=30, unique=True, null=False)
+
+class Favor(models.Model):
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+    menu_id = models.ForeignKey(Menu, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('account_id', 'menu_id')
